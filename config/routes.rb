667e1d1f683db9eb6efe32dc74e46042bd41128f
vcavallo <vcavallo@gmail.com>
2014-11-08 14:22:27 -0500

    Adds static controller & pages, fixes content margins

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ EgnaRails::Application.routes.draw do
   root 'projects#index'
   resources :projects
 
+  get 'about' => 'static#about', as: :about
+  get 'contact' => 'static#contact', as: :contact
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
