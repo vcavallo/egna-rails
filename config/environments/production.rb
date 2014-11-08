64983634c93977d647a18e940d776ff7bdcbf8fa
vcavallo <vcavallo@gmail.com>
2014-11-08 14:36:28 -0500

    Changes assets compile to true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ EgnaRails::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w( vendor/modernizr.js )
 
   # Generate digests for assets URLs.
